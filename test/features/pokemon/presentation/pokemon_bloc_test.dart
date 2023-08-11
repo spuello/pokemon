d62493345d7b1d79b48c18743c6e5218398e7359
Samuel Puello <spuellocoronado@gmail.com>
2023-08-11 10:47:12 -0400

    Add unit test to PokemonBloc for PokemonSelected event

--- test/features/pokemon/presentation/pokemon_bloc_test.dart
+++ test/features/pokemon/presentation/pokemon_bloc_test.dart
@@ -7,6 +7,9 @@ import 'package:pokemon/features/pokemon/presentation/bloc/pokemon_bloc.dart';
 import '../../../fixtures/fixture_reader.dart';
 import '../../../mocks..dart';
 
+final jsonString = fixtureFromJson('pokemon.json');
+final tPokemonList = PokemonList.fromJson(jsonString);
+
 void main() {
   late MockPokemonRepository mockPokemonRepository;
 
@@ -17,15 +20,12 @@ void main() {
   PokemonBloc buildBloc() => PokemonBloc(repository: mockPokemonRepository);
 
   group('PokemonListLoaded', () {
-    final jsonString = fixtureFromJson('pokemon.json');
-    final list = PokemonList.fromJson(jsonString);
-
     blocTest<PokemonBloc, PokemonState>(
       'Emits a list of Pokemon upon successful retrieval from the backend',
       build: buildBloc,
       setUp: () {
         when(() => mockPokemonRepository.getAll())
-            .thenAnswer((_) async => list.items);
+            .thenAnswer((_) async => tPokemonList.items);
       },
       act: (bloc) => bloc.add(PokemonListLoaded()),
       expect: () => [
@@ -49,6 +49,24 @@ void main() {
             .having((p0) => p0.status, 'status', equals(Status.loading)),
         isA<PokemonState>()
             .having((p0) => p0.status, 'status', equals(Status.failure))
+      ],
+    );
+  });
+
+  group('PokemonSelected', () {
+    blocTest<PokemonBloc, PokemonState>(
+      'Emits PokemonState containing the selected Pokemon',
+      build: buildBloc,
+      setUp: () {
+        when(() => mockPokemonRepository.getAll()).thenThrow(Exception());
+      },
+      act: (bloc) => bloc.add(PokemonSelected(item: tPokemonList.items.first)),
+      expect: () => [
+        isA<PokemonState>().having(
+          (p0) => p0.selected,
+          'selected',
+          equals(tPokemonList.items.first),
+        ),
       ],
     );
   });
